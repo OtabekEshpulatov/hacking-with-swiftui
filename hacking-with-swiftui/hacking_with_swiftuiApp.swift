6409b5d76aac116ddfed6e8010b519595d93ad0c
Otabek.E <otabekjan.eshpulatov@gmail.com>
2025-02-10 09:04:40 +0100

    Initial Commit

---
+++ hacking-with-swiftui/hacking_with_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  hacking_with_swiftuiApp.swift
+//  hacking-with-swiftui
+//
+//  Created by kebato OS on 10/02/25.
+//
+
+import SwiftUI
+
+@main
+struct hacking_with_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
